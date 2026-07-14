47c6b6475505ceca3c407856d4c4a9b36b2fe641
Bedirhan Sak <bedirhansak@gmail.com>
2026-07-14 12:36:59 +0300

    Fix update endpoint

--- Todolist2/Todolist2/Application/Commands/UpdateTodoCommand.cs
+++ Todolist2/Todolist2/Application/Commands/UpdateTodoCommand.cs
@@ -1,11 +1,8 @@
 ﻿namespace Todolist2.Application.Commands;
 
-    public class UpdateTodoCommand
-    {
-        [System.Text.Json.Serialization.JsonIgnore]
-        public string Title { get; set; } = string.Empty;
-        [System.ComponentModel.DataAnnotations.MaxLength(500)]
-        public string Description { get; set; } = string.Empty;
-        public bool IsCompleted { get; set; }
-
-    }
+public class UpdateTodoCommand
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+}
