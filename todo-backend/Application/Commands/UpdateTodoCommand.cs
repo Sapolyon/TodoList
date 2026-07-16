f65d8242df94e06e8ba632763955f71d4a869487
Bedirhan Sak <bedirhansak@gmail.com>
2026-07-16 10:57:53 +0300

    Final project cleanup

--- todo-backend/Application/Commands/UpdateTodoCommand.cs
+++ todo-backend/Application/Commands/UpdateTodoCommand.cs
@@ -2,7 +2,12 @@
 
 public class UpdateTodoCommand
 {
+    [System.ComponentModel.DataAnnotations.Required]
+    [System.ComponentModel.DataAnnotations.MaxLength(100)]
     public string Title { get; set; } = string.Empty;
+
+    [System.ComponentModel.DataAnnotations.MaxLength(500)]
     public string Description { get; set; } = string.Empty;
+
     public bool IsCompleted { get; set; }
 }
